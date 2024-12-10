c970a04ee11695c0c47a33d8e0a9bcf3ab9024ad
anoleose <65123548+anoleose@users.noreply.github.com>
2024-12-10 18:44:52 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
 
+from fastapi import FastAPI
+from app.routers import authors, books, borrows
+from app.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(authors.router, prefix="/authors", tags=["Authors"])
+app.include_router(books.router, prefix="/books", tags=["Books"])
+app.include_router(borrows.router, prefix="/borrows", tags=["Borrows"])
