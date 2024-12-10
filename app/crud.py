305a841047c2868fac0ff2914024206075befae6
anoleose <65123548+anoleose@users.noreply.github.com>
2024-12-10 17:09:10 +0500

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from app.models import Author, Book, Borrow
-from app.schemas import AuthorCreate, BookCreate, BorrowCreate
+from app.schemas import AuthorCreate, BookCreate, BorrowCreate, AuthorUpdate, BookUpdate, BorrowUpdate
 
 # CRUD for Authors
 def create_author(db: Session, author: AuthorCreate):
@@ -20,9 +20,9 @@ def get_author_by_id(db: Session, author_id: int):
 def update_author(db:Session, author_id: int):
     author = get_author_by_id(db=db, author_id=author_id)
     if author
-    	author.title = AuthorCreate.first_name
-    	author.surname = AuthorCreate.surname
-    	author.date_of_birth = AuthorCreate.date_of_birth
+    	author.first_name = AuthorUpdate.first_name
+    	author.surname = AuthorUpdate.surname
+    	author.date_of_birth = AuthorUpdate.date_of_birth
     	db.commit()
     	db.refresh(author)
     	return author
@@ -36,35 +36,36 @@ def delete_author(db: Session, author_id: int):
 
 
 # CRUD for books
-def create_author(db: Session, author: AuthorCreate):
-    db_author = Author(**author.dict())
-    db.add(db_author)
+def create_book(db: Session, book: BookCreate):
+    db_book = Book(**book.dict())
+    db.add(db_book)
     db.commit()
-    db.refresh(db_author)
-    return db_author
+    db.refresh(db_book)
+    return db_book
 
-def get_authors(db: Session):
-    return db.query(Author).all()
+def get_books(db: Session):
+    return db.query(Book).all()
 
-def get_author_by_id(db: Session, author_id: int):
-    return db.query(Author).filter(Author.id == author_id).first()
+def get_book_by_id(db: Session, book_id: int):
+    return db.query(Book).filter(Book.id == book_id).first()
 
 
-def update_author(db:Session, author_id: int):
-    author = get_author_by_id(db=db, author_id=author_id)
-    if author
-    	author.title = AuthorCreate.first_name
-    	author.surname = AuthorCreate.surname
-    	author.date_of_birth = AuthorCreate.date_of_birth
+def update_book(db:Session, book_id: int):
+    book = get_book_by_id(db=db, book_id=book_id)
+    if book:
+    	book.title = BookUpdate.title
+    	book.description = BookUpdate.description
+    	book.author_id = BookUpdate.author_id
+	book.copies_available = BookUpdate.copies_available 
     	db.commit()
-    	db.refresh(author)
-    	return author
+    	db.refresh(book)
+    	return book
 	
 
-def delete_author(db: Session, author_id: int):
-    author = get_author_by_id(db, author_id)
-    if author:
-        db.delete(author)
+def delete_book(db: Session, book_id: int):
+    book = get_author_by_id(db, book_id)
+    if book:
+        db.delete(book)
         db.commit()
 
 
