82548d91b94ce23da6fd1900dbc98b5c734287a5
anoleose <65123548+anoleose@users.noreply.github.com>
2024-12-10 18:55:21 +0500

    Update test_authors.py

--- app/tests/test_authors.py
+++ app/tests/test_authors.py
@@ -4,3 +4,14 @@ def test_create_author(client):
     response = client.post("/authors/", json={"first_name": "John", "surname": "Doe", "date_of_birth": "1980-01-01"})
     assert response.status_code == 200
     assert response.json()["first_name"] == "John"
+
+
+def test_create_author(client):
+    response = client.post("/authors/", json={
+        "first_name": "John",
+        "surname": "Doe",
+        "date_of_birth": "1980-01-01"
+    })
+    assert response.status_code == 200
+    assert response.json()["first_name"] == "John"
+
