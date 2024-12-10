47981adbe95b3386386fd4f916c13cf8fa554f4b
anoleose <65123548+anoleose@users.noreply.github.com>
2024-12-10 17:56:14 +0500

    Update authors.py

--- app/routers/authors.py
+++ app/routers/authors.py
@@ -19,18 +19,22 @@ def get_author(author_id: int, db: Session = Depends(get_db)):
     if not author:
         raise HTTPException(status_code=404, detail="Author not found")
     return author
-  
-@router.post("/author/update/{id}", response_model=schemas.Author)
+
+
+@router.put("/author/update/{id}", response_model=schemas.Author)
 def update_author(author_id: int, db: Session = Depends(get_db)):
     author = crud.update_author(db, author_id)
     if not author:
         raise HTTPException(status_code=404, detail="Author not found")
     return author
+
   
-@router.delete("/author/delete/{id}")
-def delete(author_id:int, db:Session=Depends(get_db)):
-	crud.delete_author(db, author_id=id)
-	return Response(code=200,status="ok", message="success deleted data", result=_post).dict(exclude_none=True)
+@router.delete("/author/delete/{id}", response_model=schemas.Author)
+def delete_author(author_id:int, db:Session=Depends(get_db)):
+    author = crud.delete_author(db, author_id=id)
+    if not author:
+        raise HTTPException(status_code=404, detail="Author not found") 
+    return author
 
 
 
@@ -38,7 +42,7 @@ def delete(author_id:int, db:Session=Depends(get_db)):
 #Router for books
 @router.post("/create-book", response_model=schemas.Book)
 def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
-    return crud.create_book(db, author)
+    return crud.create_book(db, book)
 
 @router.get("/books", response_model=list[schemas.Book])
 def get_books(db: Session = Depends(get_db)):
@@ -51,14 +55,16 @@ def get_book(book_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Book not found")
     return book
   
-@router.post("/book/update/{id}", response_model=schemas.Book)
+@router.put("/book/update/{id}", response_model=schemas.Book)
 def update_book(book_id: int, db: Session = Depends(get_db)):
     book = crud.update_book(db, book_id)
     if not book:
         raise HTTPException(status_code=404, detail="Book not found")
     return book
 
-@router.delete("/{id}")
+@router.delete("/book/delete/{id}")
 def delete(id:int, db:Session=Depends(get_db)):
-	crud.remove_post(db, post_id=id)
-	return Response(code=200,status="ok", message="success deleted data", result=_post).dict(exclude_none=True)
+    book = crud.delete_book(db, book_id=id)
+    if not book:
+        return raise HTTPException(status_code=404, detail="Book not found")
+    return book
