c9f71c9e715c93d4bc61d676e2b0b39f3f7ef100
anoleose <65123548+anoleose@users.noreply.github.com>
2024-12-10 18:40:47 +0500

    Update borrows.py

--- app/routers/borrows.py
+++ app/routers/borrows.py
@@ -6,31 +6,31 @@ from app.database import get_db
 
 
 #Router for books
-@router.post("/create-book", response_model=schemas.Book)
-def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
-    return crud.create_book(db, book)
+@router.post("/create-borrow", response_model=schemas.Borrow)
+def create_borrow(borrow: schemas.BorrowCreate, db: Session = Depends(get_db)):
+    return crud.create_borrow(db, borrow)
 
-@router.get("/books", response_model=list[schemas.Book])
-def get_books(db: Session = Depends(get_db)):
-    return crud.get_books(db)
+@router.get("/borrows", response_model=list[schemas.Borrow])
+def get_borrows(db: Session = Depends(get_db)):
+    return crud.get_borrows(db)
 
-@router.get("/books/{id}", response_model=schemas.Book)
-def get_book(book_id: int, db: Session = Depends(get_db)):
-    book = crud.get_book(db, book_id)
-    if not book:
-        raise HTTPException(status_code=404, detail="Book not found")
-    return book
+@router.get("/borrow/{id}", response_model=schemas.Borrow)
+def get_borrow(borrow_id: int, db: Session = Depends(get_db)):
+    borrow = crud.get_borrow(db, borrow_id)
+    if not borrow:
+        raise HTTPException(status_code=404, detail="Borrow not found")
+    return borrow
   
-@router.put("/book/update/{id}", response_model=schemas.Book)
-def update_book(book_id: int, db: Session = Depends(get_db)):
-    book = crud.update_book(db, book_id)
-    if not book:
-        raise HTTPException(status_code=404, detail="Book not found")
-    return book
+@router.put("/borrow/update/{id}", response_model=schemas.Borrow)
+def update_borrow(borrow_id: int, db: Session = Depends(get_db)):
+    borrow = crud.update_borrow(db, borrow_id)
+    if not borrow:
+        raise HTTPException(status_code=404, detail="Borrow not found")
+    return borrow
 
-@router.delete("/book/delete/{id}")
-def delete(id:int, db:Session=Depends(get_db)):
-    book = crud.delete_book(db, book_id=id)
-    if not book:
-        return raise HTTPException(status_code=404, detail="Book not found")
-    return book
+@router.delete("/borrow/delete/{id}")
+def delete_borrow(id:int, db:Session=Depends(get_db)):
+    borrow = crud.delete_borrow(db, borrow_id=id)
+    if not borrow:
+        return raise HTTPException(status_code=404, detail="Borrow not found")
+    return borrow
