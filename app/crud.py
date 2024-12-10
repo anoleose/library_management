1e15a3bdcf9dad0c1eccd333aeea365f73092f1b
anoleose <65123548+anoleose@users.noreply.github.com>
2024-12-10 16:40:34 +0500

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -16,6 +16,17 @@ def get_authors(db: Session):
 def get_author_by_id(db: Session, author_id: int):
     return db.query(Author).filter(Author.id == author_id).first()
 
+
+def update_author(db:Session, author_id: int):
+	get_author = get_author_by_id(db=db, author_id=author_id)
+	get_author.title = author.first_name
+	get_author.surname = author.surname
+    get_author.date_of_birth = author.date_of_birth
+	db.commit()
+	db.refresh(get_author)
+	return get_author
+	
+
 def delete_author(db: Session, author_id: int):
     author = get_author_by_id(db, author_id)
     if author:
